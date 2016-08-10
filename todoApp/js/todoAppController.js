f44b393bff37492631a670eee5aa4bef8aaac3c9
Tien Lam Thi <lttien@tma.com.vn>
2016-08-10 15:13:51 +0700

    add function setStorage()

--- todoApp/js/todoAppController.js
+++ todoApp/js/todoAppController.js
@@ -1,105 +1,114 @@
-var todoapp = angular.module( "todoApp", ['ui.router'] );
+var todoapp = angular.module( 'todoApp', ['ui.router'] );
 
 todoapp.config(function($stateProvider, $urlRouterProvider){
 
 	$stateProvider
-	    .state('home', {
-	      url: '/',
-	      templateUrl: 'todoApp.html'
-	    })
-	    .state('todo', {
-	      url: '/:status',
-	      templateUrl: 'todoApp.html',
-	      controller: function($scope, $stateParams) {
-	        $scope.status = $stateParams.status;
-	        console.log($scope.status);
-	      }
-	    });
-	    
-	  $urlRouterProvider.otherwise('/');
+		.state('home', {
+			url: '/',
+			templateUrl: 'todoApp.html'
+		})
+		.state('todo', {
+			url: '/:status',
+			templateUrl: 'todoApp.html',
+			controller: function($scope, $stateParams) {
+				$scope.status = $stateParams.status;
+				console.log($scope.status);
+			}
+		});
+	
+	$urlRouterProvider.otherwise('/');
 });
 
 todoapp.controller("todoAppController",function($scope, $stateParams){
 
-		$scope.checkboxAll = false;
-		$scope.lstAll = JSON.parse(localStorage.getItem('locname') || "[]");
-		$scope.lstCompleted = [];
-		$scope.stateParams = $stateParams;
+	$scope.checkboxAll = false;
+	$scope.lstAll = JSON.parse(localStorage.getItem('todolist') || "[]");
+	$scope.lstCompleted = [];
+	$scope.stateParams = $stateParams;
 
-		$scope.$watch(
-			function () {
-				$scope.completedCount = 0;
-				for(i = 0 ; i < $scope.lstAll.length ; i++){
-					if($scope.lstAll[i].checked){
-						$scope.completedCount ++;
-					}
+	function setStorage(){
+		localStorage.setItem('todolist', JSON.stringify($scope.lstAll));
+	}
+
+	$scope.$watch(
+		function () {
+			$scope.completedCount = 0;
+			for(i = 0 ; i < $scope.lstAll.length ; i++){
+				if($scope.lstAll[i].checked){
+					$scope.completedCount ++;
 				}
 			}
-		);
+		}
+	);
 
-		$scope.$watchCollection('stateParams', function(){
-    		var status = $scope.status = $stateParams.status || '';
-			$scope.typeFilter = (status === 'active') ?
-				{ checked: false } : (status === 'completed') ?
-				{ checked: true } : {};
+	$scope.$watchCollection('stateParams', function(){
+		var status = $scope.status = $stateParams.status || '';
+		$scope.typeFilter = (status === 'active') ?
+			{ checked: false } : (status === 'completed') ?
+			{ checked: true } : {};
 
+	});
+
+	$scope.addTodo = function(keyEvent){
+		var item = {
+			value: $scope.new,
+			checked: false
+		};
+		if (keyEvent.which === 13){
+			if($scope.new != '' && $scope.new != '	'){
+				$scope.lstAll.push(item);
+				$scope.new = '';
+				setStorage();
+			}
+		}
+	};
+
+
+	$scope.remove = function(item){
+		$scope.lstAll.splice($scope.lstAll.indexOf(item), 1);
+		setStorage();
+	};
+
+	$scope.addAllCompleted = function(checked){
+		for(i = 0 ; i< $scope.lstAll.length ; i++){
+			if($scope.lstAll[i].checked !== checked){
+				$scope.changeList($scope.lstAll[i], checked);
+			}
+		}
+	};
+
+	$scope.changeList = function(item, checked){
+		var index = $scope.lstAll.indexOf(item);
+		if(angular.isDefined(checked)){
+			$scope.lstAll[index].checked = checked;
+		}
+
+		setStorage();
+	};
+
+	$scope.updateTodo = function(item, value){
+		var indexUpdate = $scope.lstAll.indexOf(item);
+		if($scope.lstAll[indexUpdate].value !== value){
+			$scope.lstAll[$scope.lstAll.indexOf(item)].value = value;
+		}
+		setStorage();
+	};
+
+	$scope.removeAll = function(){
+		for(i = 0; i< $scope.lstAll.length; i++){
+			if($scope.lstAll[i].checked){
+				$scope.lstCompleted.push($scope.lstAll[i]);
+			}
+		}
+
+		angular.forEach($scope.lstCompleted, function (value, index) {
+			$scope.lstAll.splice($scope.lstAll.indexOf(value), 1);
 		});
 
-		$scope.addTodo = function(keyEvent){
-			var item = {
-				value: $scope.new,
-				checked: false
-			};
-			if (keyEvent.which === 13){
-				if($scope.new != '' && $scope.new != '	'){
-					$scope.lstAll.push(item);
-				 	localStorage.setItem('locname', JSON.stringify($scope.lstAll));
-					$scope.new = '';
-				}
-			}
-		};
+		$scope.lstCompleted = [];
 
+		setStorage();
+	};
 
-	 	$scope.remove = function(item){
-	 		$scope.lstAll.splice($scope.lstAll.indexOf(item), 1);
-	 		localStorage.setItem('locname', JSON.stringify($scope.lstAll));
-		};
-
-		$scope.addAllCompleted = function(checked){
-	  		for(i = 0 ; i< $scope.lstAll.length ; i++){
-	  			if($scope.lstAll[i].checked !== checked){
-	  				$scope.changeList($scope.lstAll[i], checked);
-	  			}
-	  		}
-		};
-
-		$scope.changeList = function(item, checked){
-			var index = $scope.lstAll.indexOf(item);
-	  		if(angular.isDefined(checked)){
-	  			$scope.lstAll[index].checked = checked;
-	  		}
-	  		localStorage.setItem('locname', JSON.stringify($scope.lstAll));
-		};
-
-		$scope.updateTodo = function(item, value){
-			var indexUpdate = $scope.lstAll.indexOf(item);
-			if($scope.lstAll[indexUpdate].value !== value){
-				$scope.lstAll[$scope.lstAll.indexOf(item)].value = value;
-			}
-			localStorage.setItem('locname', JSON.stringify($scope.lstAll));
-		};
-
-		$scope.removeAll = function(){
-	  		for(i = 0; i< $scope.lstAll.length; i++){
-	  			if($scope.lstAll[i].checked){
-	  				$scope.lstCompleted.push($scope.lstAll[i]);
-	  			}
-	  		}
-	  		angular.forEach($scope.lstCompleted, function (value, index) {
-	            $scope.lstAll.splice($scope.lstAll.indexOf(value), 1);
-	        });
-	        $scope.lstCompleted = [];
-	        localStorage.setItem('locname', JSON.stringify($scope.lstAll));
-		};		
 });
 
