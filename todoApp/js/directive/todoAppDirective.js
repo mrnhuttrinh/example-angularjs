116ab54f18afcad7e94c747684cbba8a437ec7be
Tien Lam Thi <lttien@tma.com.vn>
2016-08-18 18:37:07 +0700

    design app using bootstrap and using require js in angualrjs

--- todoApp/js/directive/todoAppDirective.js
+++ todoApp/js/directive/todoAppDirective.js
@@ -2,8 +2,8 @@ define(function() {
 	var moduleName = function(){
 
 		var template = '<form class="todo-form" ng-submit="add()">'+
-			'<input class="new-todo" placeholder="What needs to be done?" ng-model="new"  autofocus />'+
-			'</form>';
+						'<input class="form-control new-todo"  placeholder="What needs to be done?" ng-model="new"  autofocus />'+
+						'</form>';
 
 		return  {
 			restrict: 'EA',
